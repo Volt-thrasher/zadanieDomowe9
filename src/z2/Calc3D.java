78027f63bf558c9fb8d5588da8d193003cc733cf
Volt <sonianadashkevich@gmail.com>
2019-11-05 16:50:00 +0100

    zadanie 1 dodano hashCode, poprawione 2 i 3

--- src/z2/Calc3D.java
+++ src/z2/Calc3D.java
@@ -1,4 +1,14 @@
 package z2;
 
 public interface Calc3D {
+
+    public static double ballVolume(Ball ball) {
+        double volume = 3.14*Math.pow(ball.getRadius(),3)*4/3;
+        return volume;
+    }
+
+    public static double cubeVolume (Cube cube) {
+        double volume = Math.pow(cube.getSideA(),3);
+        return volume;
+    }
 }
--- src/z2/Cube.java
+++ src/z2/Cube.java
@@ -2,8 +2,6 @@ package z2;
 
 public class Cube extends Shape3D {
     private double sideA;
-    private  double sideB;
-    private double sideC;
 
     public double getSideA() {
         return sideA;
@@ -13,25 +11,7 @@ public class Cube extends Shape3D {
         this.sideA = sideA;
     }
 
-    public double getSideB() {
-        return sideB;
-    }
-
-    public void setSideB(double sideB) {
-        this.sideB = sideB;
-    }
-
-    public double getSideC() {
-        return sideC;
-    }
-
-    public void setSideC(double sideC) {
-        this.sideC = sideC;
-    }
-
-    public Cube(double sideA, double sideB, double sideC) {
+    public Cube(double sideA) {
         this.sideA = sideA;
-        this.sideB = sideB;
-        this.sideC = sideC;
     }
 }
